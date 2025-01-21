aaefc54c83ccf4d755bc3140cdb0549c6c78940c
Charles Tack <gfaxxlos@students.codeinstitute.net>
2025-01-21 20:05:35 +0000

    add form

---
+++ tasks/forms.py
@@ -0,0 +1,12 @@
+from Django import forms
+from .models import Task
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'due_date', 'category',]
+
+        widgets = {
+            'due_date': forms.DateInput(attrs={'type': 'date'})
+        }
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import TaskForm
 from .models import Task
 
 def home(request):
     """
-    A view to display tasks to do and completed tasks
+    A view to display the tasks to do and the completed tasks
     with the tasks due soonest at the top
     """
 
-    tasks_todo = Task.objects.filter(completed=False).order_by('due_date')
-    tasks_done = Task.objects.filter(completed=True).order_by('due_date')
-    return render(request, 'tasks/index.html', {'tasks_todo': tasks_todo, 'tasks_done': tasks_done})
+    to_do_tasks = Task.objects.filter(completed=False).order_by('due_date')
+    done_tasks = Task.objects.filter(completed=True).order_by('-due_date')
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TaskForm()
+
+    context = {
+        'to_do_tasks': to_do_tasks,
+        'done_tasks': done_tasks,
+        'form': form,
+    }
+
+    return render(request, 'tasks/index.html', context)
